c6f9d008b39d90df1b696aae7ad3be54936ba502
ZagelEnterprise <Zagel.Enterprise@gmail.com>
2017-06-03 02:09:34 +0200

    Add files via upload

---
+++ FrmCaptcha.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Captcha
+{
+    partial class FrmCaptcha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaptchaPathBox = new System.Windows.Forms.PictureBox();
+            this.BtnVerify = new System.Windows.Forms.Button();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.TxtVerify = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptchaPathBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CaptchaPathBox
+            // 
+            this.CaptchaPathBox.Location = new System.Drawing.Point(32, 23);
+            this.CaptchaPathBox.Name = "CaptchaPathBox";
+            this.CaptchaPathBox.Size = new System.Drawing.Size(139, 60);
+            this.CaptchaPathBox.TabIndex = 0;
+            this.CaptchaPathBox.TabStop = false;
+            // 
+            // BtnVerify
+            // 
+            this.BtnVerify.Location = new System.Drawing.Point(105, 127);
+            this.BtnVerify.Name = "BtnVerify";
+            this.BtnVerify.Size = new System.Drawing.Size(64, 30);
+            this.BtnVerify.TabIndex = 1;
+            this.BtnVerify.Text = "Verify";
+            this.BtnVerify.UseVisualStyleBackColor = true;
+            this.BtnVerify.Click += new System.EventHandler(this.BtnVerify_Click);
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(33, 127);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(64, 30);
+            this.BtnRefresh.TabIndex = 2;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // TxtVerify
+            // 
+            this.TxtVerify.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtVerify.Location = new System.Drawing.Point(32, 89);
+            this.TxtVerify.Name = "TxtVerify";
+            this.TxtVerify.Size = new System.Drawing.Size(139, 27);
+            this.TxtVerify.TabIndex = 3;
+            // 
+            // FrmCaptcha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 174);
+            this.Controls.Add(this.TxtVerify);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.BtnVerify);
+            this.Controls.Add(this.CaptchaPathBox);
+            this.Name = "FrmCaptcha";
+            this.Text = "CaptchaPath";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmCaptcha_FormClosed);
+            this.Load += new System.EventHandler(this.FrmCaptcha_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CaptchaPathBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CaptchaPathBox;
+        private System.Windows.Forms.Button BtnVerify;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.TextBox TxtVerify;
+    }
+}
+
---
+++ FrmCaptcha.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Captcha
+{
+    public partial class FrmCaptcha : Form
+    {         
+        Random rand = new Random();
+        string CaptchaCode,CaptchaPath;
+        string CaptchaAlphabets;
+        public FrmCaptcha()
+        {
+            InitializeComponent();
+            CaptchaPath = "D:/CaptchaImage.bmp";
+            CaptchaAlphabets = "abcdefghijklmnopqrstuvwxyz1234567890";
+        }
+
+        private void CreateImage()
+        {
+            string Code = GetRandomText();
+            Bitmap bitmap = new Bitmap(200, 50, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(Color.Yellow);
+            Rectangle rect = new Rectangle(0, 0, 200, 50);
+            SolidBrush b = new SolidBrush(Color.Black);
+            SolidBrush White = new SolidBrush(Color.White);
+            int counter = 0;
+            g.DrawRectangle(pen, rect);
+            g.FillRectangle(b, rect);
+
+            for (int i = 0; i < Code.Length; i++)
+            {
+                g.DrawString(Code[i].ToString(), new Font("Georgia", 10 + rand.Next(14, 18)), White, new PointF(10 + counter, 10));
+                counter += 20;
+            }
+            DrawRandomLines(g);
+            if (File.Exists(CaptchaPath))
+            {
+                try
+                {
+                    File.Delete(CaptchaPath);
+                    bitmap.Save(CaptchaPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                bitmap.Save(CaptchaPath);
+            }
+            g.Dispose();
+            bitmap.Dispose();
+            CaptchaPathBox.Image = Image.FromFile(CaptchaPath);
+        }
+
+        private void DrawRandomLines(Graphics g)
+        {
+            SolidBrush green = new SolidBrush(Color.Green);
+            for (int i = 0; i < 20; i++)
+            {
+                g.DrawLines(new Pen(green, 2), GetRandomPoints());
+            }
+        }
+
+        private Point[] GetRandomPoints()
+        {
+            Point[] points = { new Point(rand.Next(10, 150), rand.Next(10, 150)), new Point(rand.Next(10, 100), rand.Next(10, 100)) };
+            return points;
+        }
+
+        private string GetRandomText()
+        {
+            StringBuilder randomText = new StringBuilder();
+            if (String.IsNullOrEmpty(CaptchaCode))
+            {
+                Random r = new Random();
+                for (int j = 0; j <= 5; j++)
+                {
+                    randomText.Append(CaptchaAlphabets[r.Next(CaptchaAlphabets.Length)]);
+                }
+                CaptchaCode = randomText.ToString();
+            }
+            return CaptchaCode;
+        }
+
+        private void FrmCaptcha_Load(object sender, EventArgs e)
+        {
+            CreateImage();
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            CaptchaPathBox.Image.Dispose();
+            CaptchaCode = "";
+            CreateImage();
+        }
+
+        private void BtnVerify_Click(object sender, EventArgs e)
+        {
+            if (TxtVerify.Text == CaptchaCode.ToString())
+            {
+                File.Delete(CaptchaPath);
+                MessageBox.Show("تحقق ناجح ");
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("تحقق فاشل");
+            }
+        }
+
+
+    }
+}
